245de6e522f6e167554758ddd6ef40fa5ab20d53
ReverberateCoding <121223374+ReverberateCoding@users.noreply.github.com>
2024-04-30 10:56:00 +0000

    Completed simple flower display website

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+flowers = [
+    {
+        "name": "Mummy",
+        "price": "$99.99",
+        "image_link": "static/images/flower_images/flower_image2.jpg",
+    },
+    {
+        "name": "Hello",
+        "price": "$34.99",
+        "image_link": "static/images/flower_images/flower_image3.jpg",
+    },
+]
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html', flowers = flowers)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-
